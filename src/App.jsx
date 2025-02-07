07c94c054539a5a51479919773dc8cd0907db940
Jackster042 <feemail042@gmail.com>
2025-02-07 22:30:44 +0100

    Dev Environment setup / tailwind css / Header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 
+// COMPONENTS
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <main className="container">
-        <h1 className="h3 shadow-300">React + Vite</h1>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <main className="overflow-hidden">
+        <Header />
       </main>
     </>
   );
