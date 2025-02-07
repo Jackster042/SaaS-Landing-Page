8b8a5c8efe9f1a11517399ae5f12b0565cd0c6bd
Jackster042 <feemail042@gmail.com>
2025-02-07 18:28:24 +0100

    intital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="container">
+        <h1 className="h3 shadow-300">React + Vite</h1>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
